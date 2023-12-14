9c16fed04960ad9b63dbf2e8e47ad7ac38edaeb9
MoonRiverran <280720627@qq.com>
2023-12-14 17:28:33 +0800

    新增员工绩效管理页面

---
+++ src/api/system/perf.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询员工绩效列表
+export function listPerf(query) {
+  return request({
+    url: '/system/perf/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询员工绩效详细
+export function getPerf(empId) {
+  return request({
+    url: '/system/perf/' + empId,
+    method: 'get'
+  })
+}
+
+// 新增员工绩效
+export function addPerf(data) {
+  return request({
+    url: '/system/perf',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改员工绩效
+export function updatePerf(data) {
+  return request({
+    url: '/system/perf',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除员工绩效
+export function delPerf(empId) {
+  return request({
+    url: '/system/perf/' + empId,
+    method: 'delete'
+  })
+}
