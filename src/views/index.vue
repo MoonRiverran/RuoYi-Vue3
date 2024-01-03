837a3a17047e813176f36279ca5a132cad0d23fa
MoonRiverran <280720627@qq.com>
2024-01-03 10:54:26 +0800

    首页替换

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,93 +1,410 @@
 <template>
-  <div class="app-container home">
-    <el-row :gutter="20">
-    </el-row>
-    <el-row :gutter="20">
-      <el-col :sm="24" :lg="12" style="padding-left: 20px">
-        <h2>电气智能院员工绩效管理系统</h2>
-        <p>
-        </p>
-        <p>
-          <b>当前版本:</b> <span>v{{ version }}</span>
-        </p>
-        <p>
-        </p>
-      </el-col>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="工作类型" prop="workType">
+        <el-select v-model="queryParams.workType" placeholder="请选择工作类型" clearable>
+          <el-option
+              v-for="dict in work_type"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="项目类型" prop="projectType">
+        <el-select v-model="queryParams.projectType" placeholder="请选择项目类型" clearable>
+          <el-option
+              v-for="dict in project_type"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="完成日期" prop="completionDate">
+        <el-date-picker clearable
+                        v-model="queryParams.completionDate"
+                        type="date"
+                        value-format="YYYY-MM-DD"
+                        placeholder="请选择完成日期">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
 
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+            type="primary"
+            plain
+            icon="Plus"
+            @click="handleAdd"
+            v-hasPermi="['system:perf:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+            type="success"
+            plain
+            icon="Edit"
+            :disabled="single"
+            @click="handleUpdate"
+            v-hasPermi="['system:perf:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+            type="danger"
+            plain
+            icon="Delete"
+            :disabled="multiple"
+            @click="handleDelete"
+            v-hasPermi="['system:perf:remove']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+            type="warning"
+            plain
+            icon="Download"
+            @click="handleExport"
+            v-hasPermi="['system:perf:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
-    <el-divider />
+
+    <el-table v-loading="loading" :data="perfList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="员工姓名" align="center" prop="employeeName"  v-hasPermi="['system:user:add']" />
+      <el-table-column label="员工工号" align="center" prop="employeeNumber"  v-hasPermi="['system:user:add']" />
+      <el-table-column label="工作类型" width="140" align="center" prop="workType">
+        <template #default="scope">
+          <dict-tag :options="work_type" :value="scope.row.workType"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="项目类型" align="center" prop="projectType">
+        <template #default="scope">
+          <dict-tag :options="project_type" :value="scope.row.projectType"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="项目说明" align="center" prop="projectDescription">
+        <template #default="scope">
+          <div class="ellipsis-text" v-tooltip="scope.row.projectDescription">{{ scope.row.projectDescription }}</div>
+        </template>
+      </el-table-column>
+      <el-table-column label="目标" align="center" prop="goal" >
+        <template #default="scope">
+          <div class="ellipsis-text" v-tooltip="scope.row.goal">{{ scope.row.goal }}</div>
+        </template>
+      </el-table-column>
+      <el-table-column label="完成比例(%)" width="100" align="center" prop="completionRatio" />
+      <el-table-column label="工作时长(h)"  width="100" align="center" prop="workDuration" />
+      <el-table-column label="完成结果" align="center" prop="completionResult">
+        <template #default="scope">
+          <dict-tag :options="completion_result" :value="scope.row.completionResult"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="完成日期" align="center" prop="completionDate" width="180">
+        <template #default="scope">
+          <span>{{ parseTime(scope.row.completionDate, '{y}-{m}-{d}') }}</span>
+        </template>
+      </el-table-column>
+      <!--      <el-table-column label="创建者" align="center" prop="createBy" />-->
+      <!--      <el-table-column label="创建时间" align="center" prop="createTime" width="180">-->
+      <!--        <template #default="scope">-->
+      <!--          <span>{{ parseTime(scope.row.createTime, '{y}-{m}-{d}') }}</span>-->
+      <!--        </template>-->
+      <!--      </el-table-column>-->
+      <!--      <el-table-column label="更新者" align="center" prop="updateBy" />-->
+      <!--      <el-table-column label="更新时间" align="center" prop="updateTime" width="180">-->
+      <!--        <template #default="scope">-->
+      <!--          <span>{{ parseTime(scope.row.updateTime, '{y}-{m}-{d}') }}</span>-->
+      <!--        </template>-->
+      <!--      </el-table-column>-->
+      <el-table-column label="备注" align="center" prop="remark" >
+        <template #default="scope">
+          <div class="ellipsis-text" v-tooltip="scope.row.remark">{{ scope.row.remark }}</div>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template #default="scope">
+          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['system:perf:edit']">修改</el-button>
+          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['system:perf:remove']">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+        v-show="total>0"
+        :total="total"
+        v-model:page="queryParams.pageNum"
+        v-model:limit="queryParams.pageSize"
+        @pagination="getList"
+    />
+
+    <!-- 添加或修改员工绩效对话框 -->
+    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
+      <el-form ref="perfRef" :model="form" :rules="rules" label-width="100px">
+        <el-form-item label="工作类型" prop="workType">
+          <el-select v-model="form.workType" placeholder="请选择工作类型">
+            <el-option
+                v-for="dict in work_type"
+                :key="dict.value"
+                :label="dict.label"
+                :value="dict.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="项目类型" prop="projectType">
+          <el-select v-model="form.projectType" placeholder="请选择项目类型">
+            <el-option
+                v-for="dict in project_type"
+                :key="dict.value"
+                :label="dict.label"
+                :value="dict.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="项目说明" prop="projectDescription">
+          <el-input v-model="form.projectDescription" type="textarea" placeholder="请输入项目说明" />
+        </el-form-item>
+        <el-form-item label="目标" prop="goal">
+          <el-input v-model="form.goal" type="textarea" placeholder="请输入目标" />
+        </el-form-item>
+        <el-form-item label="完成比例(%)" prop="completionRatio">
+          <el-input v-model="form.completionRatio"  placeholder="请输入完成比例" />
+        </el-form-item>
+        <el-form-item label="完成结果" prop="completionResult">
+          <el-select v-model="form.completionResult" placeholder="请选择完成结果">
+            <el-option
+                v-for="dict in completion_result"
+                :key="dict.value"
+                :label="dict.label"
+                :value="dict.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="完成日期" prop="completionDate">
+          <el-date-picker clearable
+                          v-model="form.completionDate"
+                          type="date"
+                          value-format="YYYY-MM-DD"
+                          placeholder="请选择完成日期">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="工作时长(h)" prop="workDuration">
+          <el-input v-model="form.workDuration"  placeholder="请输入工作时长" />
+        </el-form-item>
+        <el-form-item label="备注" prop="remark">
+          <el-input v-model="form.remark" type="textarea" placeholder="请输入内容" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
-<script setup name="Index">
-const version = ref('1.0.0')
+<script setup name="Perf">
+import { listPerf, getPerf, delPerf, addPerf, updatePerf } from "@/api/system/perf";
 
-function goTarget(url) {
-  window.open(url, '__blank')
+const { proxy } = getCurrentInstance();
+const { work_type, project_type, completion_result } = proxy.useDict('work_type', 'project_type', 'completion_result');
+
+const perfList = ref([]);
+const open = ref(false);
+const loading = ref(true);
+const showSearch = ref(true);
+const ids = ref([]);
+const single = ref(true);
+const multiple = ref(true);
+const total = ref(0);
+const title = ref("");
+
+const data = reactive({
+  form: {},
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    workType: null,
+    projectType: null,
+    completionDate: null,
+  },
+  rules: {
+    workType: [
+      { required: true, message: "工作类型不能为空", trigger: "change" }
+    ],
+    projectType: [
+      { required: true, message: "项目类型不能为空", trigger: "change" }
+    ],
+    completionRatio: [
+      { required: true, message: "完成比列不能为空", trigger: "blur" },
+      {
+        pattern: /^[0-9]+(\.[0-9]+)?$/,
+        message: "请输入数字",
+        trigger: "blur"
+      },
+      {
+        validator: (rule, value, callback) => {
+          if (value < 0 || value > 100) {
+            callback(new Error("完成比例必须介于 0 和 100 之间"));
+          } else {
+            callback();
+          }
+        },
+        trigger: "blur"
+      } ],
+    workDuration: [
+      { required: true, message: "工作时长不能为空", trigger: "blur" },
+      {
+        pattern: /^[0-9]+(\.[0-9]+)?$/,
+        message: "请输入数字",
+        trigger: "blur"
+      }
+    ]
+  }
+});
+
+const { queryParams, form, rules } = toRefs(data);
+
+/** 查询员工绩效列表 */
+function getList() {
+  loading.value = true;
+  listPerf(queryParams.value).then(response => {
+    perfList.value = response.rows;
+    total.value = response.total;
+    loading.value = false;
+  });
 }
+
+// 取消按钮
+function cancel() {
+  open.value = false;
+  reset();
+}
+
+// 表单重置
+function reset() {
+  form.value = {
+    empId: null,
+    employeeName: null,
+    employeeNumber: null,
+    workType: null,
+    projectType: null,
+    projectDescription: null,
+    goal: null,
+    completionRatio: null,
+    completionResult: null,
+    completionDate: null,
+    workDuration: null,
+    extensionField1: null,
+    extensionField2: null,
+    extensionField3: null,
+    createBy: null,
+    createTime: null,
+    updateBy: null,
+    updateTime: null,
+    remark: null
+  };
+  proxy.resetForm("perfRef");
+}
+
+/** 搜索按钮操作 */
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+}
+
+/** 重置按钮操作 */
+function resetQuery() {
+  proxy.resetForm("queryRef");
+  handleQuery();
+}
+
+// 多选框选中数据
+function handleSelectionChange(selection) {
+  ids.value = selection.map(item => item.empId);
+  single.value = selection.length != 1;
+  multiple.value = !selection.length;
+}
+
+/** 新增按钮操作 */
+function handleAdd() {
+  reset();
+  open.value = true;
+  title.value = "添加员工绩效";
+}
+
+/** 修改按钮操作 */
+function handleUpdate(row) {
+  reset();
+  const _empId = row.empId || ids.value
+  getPerf(_empId).then(response => {
+    form.value = response.data;
+    open.value = true;
+    title.value = "修改员工绩效";
+  });
+}
+
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs["perfRef"].validate(valid => {
+    if (valid) {
+      if (form.value.empId != null) {
+        updatePerf(form.value).then(response => {
+          proxy.$modal.msgSuccess("修改成功");
+          open.value = false;
+          getList();
+        });
+      } else {
+        addPerf(form.value).then(response => {
+          proxy.$modal.msgSuccess("新增成功");
+          open.value = false;
+          getList();
+        });
+      }
+    }
+  });
+}
+
+/** 删除按钮操作 */
+function handleDelete(row) {
+  const _empIds = row.empId || ids.value;
+  proxy.$modal.confirm('是否确认删除员工绩效编号为"' + _empIds + '"的数据项？').then(function() {
+    return delPerf(_empIds);
+  }).then(() => {
+    getList();
+    proxy.$modal.msgSuccess("删除成功");
+  }).catch(() => {});
+}
+
+/** 导出按钮操作 */
+function handleExport() {
+  proxy.download('system/perf/export', {
+    ...queryParams.value
+  }, `perf_${new Date().getTime()}.xlsx`)
+}
+
+getList();
 </script>
-
-<style scoped lang="scss">
-.home {
-  blockquote {
-    padding: 10px 20px;
-    margin: 0 0 20px;
-    font-size: 17.5px;
-    border-left: 5px solid #eee;
-  }
-  hr {
-    margin-top: 20px;
-    margin-bottom: 20px;
-    border: 0;
-    border-top: 1px solid #eee;
-  }
-  .col-item {
-    margin-bottom: 20px;
-  }
-
-  ul {
-    padding: 0;
-    margin: 0;
-  }
-
-  font-family: "open sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-  font-size: 13px;
-  color: #676a6c;
-  overflow-x: hidden;
-
-  ul {
-    list-style-type: none;
-  }
-
-  h4 {
-    margin-top: 0px;
-  }
-
-  h2 {
-    margin-top: 10px;
-    font-size: 26px;
-    font-weight: 100;
-  }
-
-  p {
-    margin-top: 10px;
-
-    b {
-      font-weight: 700;
-    }
-  }
-
-  .update-log {
-    ol {
-      display: block;
-      list-style-type: decimal;
-      margin-block-start: 1em;
-      margin-block-end: 1em;
-      margin-inline-start: 0;
-      margin-inline-end: 0;
-      padding-inline-start: 40px;
-    }
-  }
+<style>
+.ellipsis-text {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 200px; /* 设置最大宽度 */
+}
+.ellipsis-text:hover {
+  white-space: normal;
+  overflow: visible;
+  text-overflow: inherit;
 }
 </style>
-
